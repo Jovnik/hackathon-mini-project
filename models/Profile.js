9339abad59117c14eee90502dcf282730345be5a
Jovnik <jovan@nikolic.net.au>
2019-10-22 14:23:59 +1100

    models updated

--- models/Profile.js
+++ models/Profile.js
@@ -1,2 +1,26 @@
 const mongoose = require('mongoose');
 
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    words: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'word'
+    }],
+    bio: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+
+})
+
+const Profile = mongoose.model('profile', ProfileSchema);
--- models/Word.js
+++ models/Word.js
@@ -1,2 +1,28 @@
 const mongoose = require('mongoose');
 
+const WordSchema = new mongoose.Schema({
+    word: {
+        type: String,
+        required: true,
+    },
+    partofspeech: {
+        type: String,
+        required: true
+    },
+    definition: {
+        type: String,
+        required: true
+    },
+    example: {
+        type: String,
+        required: true
+    },
+    bookfoundin: {
+        type: String
+    },
+    dayfound: {
+        type: Date,
+        default: Date.now
+
+    }
+})
